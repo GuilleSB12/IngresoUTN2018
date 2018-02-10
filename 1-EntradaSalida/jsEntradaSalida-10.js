0e86cb6d7e8e0271f86db22eec4027f0aa26cb90
GuilleSB12 <36248588+GuilleSB12@users.noreply.github.com>
2018-02-09 22:20:27 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
+    var importe;
+    var importe25;
+    var nuevoImporte;
+    
+    importe = parseInt(document.getElementById("importe").value);
+
+    importe25 = importe * 0.25;
+
+    nuevoImporte = importe - importe25;
+    
+    document.getElementById("resultado").value = nuevoImporte;
 	
 }
