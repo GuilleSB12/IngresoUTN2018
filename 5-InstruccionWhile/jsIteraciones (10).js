6ebc9f74b7c5b497e3bdc721dca5757a5c26c994
GuilleSB12 <36248588+GuilleSB12@users.noreply.github.com>
2018-02-23 22:16:08 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -4,14 +4,14 @@ function Mostrar()
 	var numero;
 	var seguir;
 	var diferencia;
-	var promedioPos;
-	var promedioNeg;
-	var acumuladorNeg;
-	var acumuladorPos;
-	var contadorPos;
-	var contadorNeg;
-	var acumuladorCero;
-	var contadorPares;
+	var promedioPos = 0;
+	var promedioNeg = 0;
+	var acumuladorNeg = 0;
+	var acumuladorPos = 0;
+	var contadorPos = 0;
+	var contadorNeg = 0;
+	var contadorCeros = 0;
+	var contadorPares = 0;
 
 	do
 	{
@@ -20,22 +20,51 @@ function Mostrar()
 		if ( numero > 0 )
 		{
 			contadorPos++;
+			acumuladorPos++;
 		}
 		
 		else if ( numero < 0 )
 		{
 			contadorNeg++;
+			acumuladorNeg++;
 		}
 
-		else if ( numero + 1)		
+		else
+		{
+			contadorCero++;
+		}	
 
-
-		
+		if ( numero % 2 == 0)
+		{
+			contadorPares++;
+		}
 
 
 		seguir = prompt("¿Desea continuar?");
 
 	} while ( seguir == 'si' );
+
+	if ( contadorPos != 0 ) //Le pongo un condicional xq si contador pos o neg son cero no continua el programa
+	{
+		promedioPos = acumuladorPos / contadorPos; //con esto nos aseguramos de el programa muestre cero en ves de nan o se cuelgue
+	}
+
+	if ( contadorNeg != 0 )
+	{
+		promedioNeg = acumuladorNeg / contadorNeg;
+	}
+
+	diferencia = contadorPos - contadorNeg;
+
+	document.write("1_Suma de los positivos " + acumuladorPos + "<br/>");
+	document.write("1_Suma de los negativos " + acumuladorNeg + "<br/>");
+	document.write("1_Suma de los positivos " + acumuladorPos + "<br/>");
+	document.write("1_Suma de los positivos " + acumuladorPos + "<br/>");
+	document.write("1_Suma de los positivos " + acumuladorPos + "<br/>");
+	document.write("1_Suma de los positivos " + acumuladorPos + "<br/>");
+	document.write("1_Suma de los positivos " + acumuladorPos + "<br/>");
+	document.write("1_Suma de los positivos " + acumuladorPos + "<br/>");
+	document.write("1_Suma de los positivos " + acumuladorPos + "<br/>");
 
 
 
