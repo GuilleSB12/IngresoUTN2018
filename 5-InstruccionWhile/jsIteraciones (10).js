7740195af210f0b06be51f37f4c158e16d0f285c
GuilleSB12 <36248588+GuilleSB12@users.noreply.github.com>
2018-02-23 21:03:48 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,24 +3,43 @@ function Mostrar()
 
 	var numero;
 	var seguir;
-	var diferncia;
+	var diferencia;
 	var promedioPos;
 	var promedioNeg;
 	var acumuladorNeg;
 	var acumuladorPos;
 	var contadorPos;
 	var contadorNeg;
+	var acumuladorCero;
+	var contadorPares;
 
 	do
 	{
+		numero = parseInt( prompt("Ingrese un numero: "));
+
+		if ( numero > 0 )
+		{
+			contadorPos++;
+		}
+		
+		else if ( numero < 0 )
+		{
+			contadorNeg++;
+		}
+
+		else if ( numero + 1)		
+
 
 		
 
 
-
+		seguir = prompt("¿Desea continuar?");
 
 	} while ( seguir == 'si' );
 
 
 
-}//FIN DE LA FUNCIÓN
+
+
+
+}//FIN DE LA FUNCIÓNi
